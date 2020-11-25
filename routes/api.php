5823f7e68b9e8368e989d88d7d3d7b2ec9a764ce
vlad <workvladster@gmail.com>
2020-11-25 00:22:56 -0800

    revert api route

--- routes/api.php
+++ routes/api.php
@@ -14,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('testing-the-api',function (){
-    return ['message'=>'hello api'];
-});
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
